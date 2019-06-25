d884c4ddf85f86d120e2c803ce54506dc649663d
Matthias Fuchs <meister.fuchs@gmail.com>
2019-06-25 21:41:53 +0200

    refactoring to kakao

--- src/main/java/io/spotnext/ide/Application.java
+++ src/main/java/io/spotnext/ide/Application.java
@@ -1,18 +1,169 @@
 package io.spotnext.ide;
 
-import io.spotnext.ide.ui.MainWindow;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class Application extends at.int32.sweaty.ui.Application {
+import io.spotnext.kakao.NSApplication;
+import io.spotnext.kakao.foundation.NSRect;
+import io.spotnext.kakao.foundation.NSSize;
+import io.spotnext.kakao.structs.DataGroupNode;
+import io.spotnext.kakao.structs.DataLeafNode;
+import io.spotnext.kakao.structs.NSAutoresizingMaskOptions;
+import io.spotnext.kakao.structs.NSFocusRingType;
+import io.spotnext.kakao.structs.NSImage;
+import io.spotnext.kakao.structs.NSImageName;
+import io.spotnext.kakao.structs.NSSplitViewDividerStyle;
+import io.spotnext.kakao.structs.NSWindowTitleVisibility;
+import io.spotnext.kakao.structs.Orientation;
+import io.spotnext.kakao.structs.SelectionHighlightStyle;
+import io.spotnext.kakao.support.NSOutlineViewDataSource;
+import io.spotnext.kakao.support.NSOutlineViewDelegate;
+import io.spotnext.kakao.ui.NSButton;
+import io.spotnext.kakao.ui.NSClipView;
+import io.spotnext.kakao.ui.NSOutlineView;
+import io.spotnext.kakao.ui.NSScrollView;
+import io.spotnext.kakao.ui.NSSearchField;
+import io.spotnext.kakao.ui.NSSplitView;
+import io.spotnext.kakao.ui.NSTableColumn;
+import io.spotnext.kakao.ui.NSTextField;
+import io.spotnext.kakao.ui.NSToolbar;
+import io.spotnext.kakao.ui.NSToolbarItem;
+import io.spotnext.kakao.ui.NSWindow;
+import io.spotnext.kakao.util.ThreadUtil;
 
-	private final MainWindow mainWindow;
+public class Application {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String... args) {
-		new Application();
+		ThreadUtil.performOnMainThread(() -> {
+			final var app = NSApplication.sharedApplication();
+			app.setApplicationIconImage("/Applications/Development/Eclipse.app/Contents/Resources/Eclipse.icns");
+			app.setApplicationName("JDE");
+			app.setApplicationShouldTerminateAfterLastWindowClosed(true);
+
+			final var window = new NSWindow();
+			window.setTitle("JDE");
+			window.setTitleVisibility(NSWindowTitleVisibility.Hidden);
+			window.setToolbar(createToolbar());
+
+			createSplitPane(window);
+
+			window.center();
+
+			app.onApplicationDidFinishLaunching(p -> {
+				app.activateIgnoringOtherApps(true);
+				window.makeKeyAndOrderFront(null);
+			});
+
+			app.run();
+		});
+	}
+	
+	private static void createSplitPane(NSWindow window) {
+		var bounds = window.contentViewFrame();
+
+		var splitView = new NSSplitView(bounds);
+		splitView.setDividerStyle(NSSplitViewDividerStyle.Thin);
+		splitView.setOrientation(Orientation.Vertical);
+
+		splitView.getDividerStyle();
+
+		var sidebarX = 0;
+		var sidebarY = 0;
+		var sidebarWidth = 250d;
+		var sidebarHeight = bounds.size.height.doubleValue();
+
+		var sidebar = new NSOutlineView();
+		sidebar.setSelectionHighlightStyle(SelectionHighlightStyle.SourceList);
+
+		var col1 = new NSTableColumn("Content");
+		col1.setEditable(false);
+		col1.getHeaderCell().setStringValue("Content");
+		sidebar.addTableColumn(col1);
+		sidebar.setOutlineTableColumn(col1);
+		sidebar.setTableHeaderView(null);
+
+		var root = new DataGroupNode("root");
+		var child1 = new DataLeafNode("leave");
+		root.addNodes(child1);
+		sidebar.setDataSource(new NSOutlineViewDataSource(root) {
+		});
+		sidebar.setDelegate(new NSOutlineViewDelegate() {
+		});
+
+		var sidebarRect = new NSRect(sidebarX, sidebarY, sidebarWidth, sidebarHeight);
+		var clipView = new NSClipView();
+		clipView.setAutoresizesSubviews(true);
+		clipView.setDocumentView(sidebar);
+
+		var sidebarScrollView = new NSScrollView(sidebarRect);
+		sidebarScrollView.setContentView(clipView);
+		sidebarScrollView.setAutoresizingMask(NSAutoresizingMaskOptions.MaxXMargin);
+
+		var textFieldX = sidebarX + sidebarWidth;
+		var textFieldY = sidebarY;
+		var textFieldWidth = bounds.size.width.doubleValue() - sidebarWidth;
+		var textFieldHeight = sidebarHeight;
+
+		var textField = new NSTextField(new NSRect(textFieldX, textFieldY, textFieldWidth, textFieldHeight));
+		textField.setFocusRingType(NSFocusRingType.None);
+		textField.setBordered(false);
+
+		splitView.addSubview(sidebarScrollView);
+		splitView.addSubview(textField);
+
+		// why can't I pass 200?
+//		splitView.setPosition(20d, 0);
+		splitView.adjustSubviews();
+		splitView.setAutoresizingMask(NSAutoresizingMaskOptions.HeightSizable, NSAutoresizingMaskOptions.WidthSizable);
+//		splitView.setHoldingPriorityForSubview(490, 0);
+
+		window.addSubview(splitView);
 	}
 
-	private Application() {
-		mainWindow = new MainWindow("JDE");
-		mainWindow.show();
-		setAppName("JDE");
+	public static NSToolbar createToolbar() {
+		var toolbar = new NSToolbar();
+
+		int itemIndex = 0;
+
+		var runButtonItem = new NSToolbarItem("runButtonItem");
+		runButtonItem.setLabel("Run");
+		runButtonItem.setToolTip("Build and then run the current application");
+		
+		var runButton = new NSButton(NSImageName.GoRight);
+		runButton.setFrameSize(NSSize.of(50, 0));
+		runButtonItem.setView(runButton);
+		toolbar.insertItem(runButtonItem, itemIndex++);
+		
+		var stopButtonItem = new NSToolbarItem("stopButtonItem");
+		stopButtonItem.setLabel("Stop");
+		stopButtonItem.setToolTip("Stop the current application");
+		stopButtonItem.setEnabled(false);
+		var stopButton = new NSButton(NSImageName.GoLeft);
+		stopButton.setFrameSize(NSSize.of(50, 0));
+		stopButtonItem.setView(stopButton);
+		stopButton.setEnabled(false);
+		toolbar.insertItem(stopButtonItem, itemIndex++);
+
+		var flexSpacer = NSToolbarItem.FLEXIBLE_SPACER;
+		flexSpacer.setVisible(true);
+		toolbar.insertItem(flexSpacer, itemIndex++);
+
+		var searchFieldItem = new NSToolbarItem("searchFieldItem");
+		searchFieldItem.setLabel("Search");
+		searchFieldItem.setToolTip("Search");
+		searchFieldItem.setView(new NSSearchField());
+		toolbar.insertItem(searchFieldItem, itemIndex++);
+
+		var spacer = NSToolbarItem.FIXED_SPACER;
+		spacer.setVisible(false);
+		toolbar.insertItem(spacer, itemIndex++);
+
+		var separator = NSToolbarItem.SEPARATOR;
+		separator.setVisible(false);
+		toolbar.insertItem(separator, itemIndex++);
+
+		return toolbar;
 	}
 }
